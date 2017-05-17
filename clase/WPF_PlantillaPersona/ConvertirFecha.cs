8655a731a5c79d068fdecedc746e2e61d0a33508
JuanFranCrater <juanfbenitezlopez@gmail.com>
2017-05-17 12:45:20 +0200

    Añadido enlace A lista y Plantillas

---
+++ clase/WPF_PlantillaPersona/ConvertirFecha.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-------------------------------------------------------
+using System.Globalization;     // Para Culture
+using System.Windows.Data;      // Para IValueConverter
+
+namespace JuanF.Wpf_EnlacesDataContextAClases
+{
+
+    class ConvertirFecha:IValueConverter
+    {
+        int _dato = 0;
+        public ConvertirFecha()
+        {
+            _dato = 10;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime fecha = (DateTime)value;
+            return fecha.ToShortDateString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ clase/WPF_PlantillaPersona/Imagenes/ConvertirFecha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//-------------------------------------------------------
+using System.Globalization;     // Para Culture
+using System.Windows.Data;      // Para IValueConverter
+
+namespace Wpf_EnlacesDataContextAClases.Imagenes
+{
+    class ConvertirFecha:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime fecha = (DateTime)value;
+            return fecha.ToShortDateString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
